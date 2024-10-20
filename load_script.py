6beb9cf4aa1649ca1b9befadcce39d1dc2fcb92a
daniel Foley <daf@scc1.bu.edu>
2024-10-20 19:07:33 -0400

    Updated the metadata loading script and the full text loading script

--- load_script.py
+++ load_script.py
@@ -1,14 +1,22 @@
-import requests
+
 
 import json
 
 import time
 
+import os
+
 import sys
+
+import requests
+
+
 
 
 
 def fetch_digital_commonwealth():
+
+    start = time.time()
 
     BASE_URL = "https://www.digitalcommonwealth.org/search.json?search_field=all_fields&per_page=100&q="
 
@@ -16,9 +24,35 @@ def fetch_digital_commonwealth():
 
     END_PAGE = sys.argv[2]
 
+    file_name = f"out{PAGE}_{END_PAGE}.json"
+
+    FINAL_PAGE = 13038
+
     output = []
 
+    file_path = f"/projectnb/sparkgrp/ml-bpl-rag-data/{file_name}"
+
+    # file_path = './output.json'
+
+    if os.path.exists(file_path):
+
+        with open(file_path,'r') as file:
+
+            output = json.load(file)
+
+            if int(PAGE) < (len(output) + 1):
+
+                PAGE = len(output) + 1
+
+    
+
+    if int(PAGE) >= int(END_PAGE):
+
+        return None
+
     print(f'Reading page {PAGE} up to page {END_PAGE}')
+
+    retries = 0
 
     while True:
 
@@ -40,19 +74,17 @@ def fetch_digital_commonwealth():
 
             # Save the entire output to a JSON file after each iteration
 
-            with open('output.json', 'w') as f:
+            with open(file_path, 'w') as f:
 
                 json.dump(output, f)
 
-            
-
-            # print(f"Processed and saved page {PAGE}. Total pages saved: {len(output)}")
 
 
 
-            # Check if there is a next page
 
-            # print(f"next page {data['meta']['pages']['next_page']} of type {type(data['meta']['pages']['next_page'])}")
+            # check if theres a next page
+
+            # print(len(response))
 
             if data['meta']['pages']['next_page']:
 
@@ -62,7 +94,15 @@ def fetch_digital_commonwealth():
 
                     break
 
+                elif data['meta']['pages']['next_page'] == FINAL_PAGE:
+
+                    print(f"finished page {PAGE}")
+
+                    PAGE = FINAL_PAGE
+
                 else:
+
+                    print(f"finished page {PAGE}")
 
                     PAGE = data['meta']['pages']['next_page']
 
@@ -72,13 +112,13 @@ def fetch_digital_commonwealth():
 
                 break
 
+            
 
+            retries = 0
 
             # Optional: Add a small delay to avoid overwhelming the API
 
             # time.sleep(0.5)
-
-
 
         except requests.exceptions.RequestException as e:
 
@@ -86,13 +126,17 @@ def fetch_digital_commonwealth():
 
             print(f"Processed and saved page {PAGE}. Total pages saved: {len(output)}")
 
-            break
+            retries += 1
 
+            if retries >= 5:
 
+                break
+
+    end = time.time()
+
+    print(f"Timer: {end - start}")
 
     print(f"Finished processing all pages. Total pages saved: {len(output)}")
-
-
 
 if __name__ == "__main__":
 
