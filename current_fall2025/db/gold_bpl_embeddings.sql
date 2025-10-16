e9ae7ae2179d25e2a676ce5234f31082b60f5b38
Chi hin nathan Chang <nc0926@scc-wl1.scc.bu.edu>
2025-10-15 22:31:10 -0400

    corrected silver and gold table, plus added logs to load vector script

--- current_fall2025/db/gold_bpl_embeddings.sql
+++ current_fall2025/db/gold_bpl_embeddings.sql
@@ -1,12 +1,11 @@
 CREATE TABLE IF NOT EXISTS gold.bpl_embeddings (
-    id SERIAL PRIMARY KEY,
     document_id TEXT NOT NULL,
     chunk_index INT NOT NULL,
     chunk_text TEXT NOT NULL,
     embedding VECTOR(768),
+    PRIMARY KEY (document_id, chunk_index)
 );
 
 CREATE INDEX IF NOT EXISTS bpl_embeddings_hnsw_idx
 ON gold.bpl_embeddings
 USING hnsw (embedding vector_cosine_ops);
-
