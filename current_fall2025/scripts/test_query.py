45ca3e1caf270d0e07ec13e3280edafe6375a26b
Chi hin nathan Chang <nc0926@scc-wl1.scc.bu.edu>
2025-10-21 21:18:43 -0400

    completely fixed the response error from LLM

---
+++ current_fall2025/scripts/test_query.py
@@ -0,0 +1,29 @@
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%H:%M:%S"
+)
+
+from dotenv import load_dotenv
+import psycopg2, os
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_openai import ChatOpenAI
+from RAG import RAG
+
+load_dotenv()
+
+conn = psycopg2.connect(
+    host=os.getenv("PGHOST"),
+    port=os.getenv("PGPORT"),
+    database=os.getenv("PGDATABASE"),
+    user=os.getenv("PGUSER"),
+    password=os.getenv("PGPASSWORD"),
+    sslmode=os.getenv("PGSSLMODE", "prefer")
+)
+
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+answer, docs = RAG(llm, conn, embeddings, query="What was the Great Fire of Boston in 1872?")
+print(answer)
